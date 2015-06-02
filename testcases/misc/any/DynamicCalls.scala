41f587de693db4f738be996300fa612f83c9c5ad
Romain Ruetschi <romain.ruetschi@gmail.com>
2015-06-03 00:33:02 +0200

    Update DynamicCalls testcase.

--- testcases/misc/any/DynamicCalls.scala
+++ testcases/misc/any/DynamicCalls.scala
@@ -1,5 +1,6 @@
 
 import leon.lang._
+import leon.lang.string._
 import leon.lang.any._
 
 import leon.annotation._
@@ -7,13 +8,25 @@ import scala.language.implicitConversions
 
 object DynamicCalls {
 
-  val anInt: Any = 12
-  val anotherInt: Any = 20
+  // val anInt: Any = 12
+  // val anotherInt: Any = 20
+  // val result = anInt + anotherInt
 
-  val result = anInt + anotherInt
+  // def add(a: Any, b: Any): Any = {
+  //   a + b - b
+  // } ensuring { res => res == a }
 
-  def add(a: Any, b: Any): Any = {
-    a + b - b
-  } ensuring { res => res == a }
+  def size(x: Any): BigInt = x match {
+    case b: BigInt => b
+    case s: String => s.size
+    case _         => 0
+  }
+
+  val a: BigInt = 2
+  val b: BigInt = 3
+
+  def lemma1(a: Any, b: Any) = {
+    size(a * b) == size(a) * size(b)
+  }.holds
 
 }
